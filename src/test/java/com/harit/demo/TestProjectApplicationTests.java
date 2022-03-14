a465d8ba160b504a6ef218b93f35cec63c1decfd
haritgrover <haritgrover@gmail.com>
2022-03-14 15:33:41 +0530

    first commit

---
+++ src/test/java/com/harit/demo/TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
